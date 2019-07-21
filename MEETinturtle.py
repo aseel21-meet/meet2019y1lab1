b2ff54f98e5411175ad769af14268d3d5b400b51
aseel21-meet <aseel21@meet.mit.edu>
2019-07-21 09:34:36 -0400

    Stopping here

---
+++ MEETinturtle.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,100)
+turtle.goto(-150,-100)
+turtle.goto(-100,100)
+turtle.goto(-100,-100)
+turtle.penup()
+turtle.goto(-100+50,-100)
+
+turtle.mainloop()
